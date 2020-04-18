e1a2726dc0548dc0d707c0332ee081a23cd5ea4c
Sidney Bovet <sidney.bovet@gmail.com>
2020-04-18 16:15:15 +0200

    Add numpad

--- keyboards/bm60rgb/keymaps/belgorath/keymap.c
+++ keyboards/bm60rgb/keymaps/belgorath/keymap.c
@@ -26,16 +26,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
       C(S(KC_ESC)),KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______,     KC_MPLY, KC_VOLU, KC_VOLD, KC_MNXT, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END, _______, _______, _______,
-        _______,  C(G(KC_LEFT)), C(G(KC_RGHT)), _______, _______, _______, _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______, _______,
+        _______,  _______, _______, C(G(KC_LEFT)), C(G(KC_RGHT)), _______, _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______, _______,
         _______,           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        KC_PGUP, _______,
         _______,   _______,   _______,                      _______,                              _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
     [2] = LAYOUT(
         RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PWR,
-        _______,     RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,
-        _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,            KC_F5,   S(KC_F11),  KC_F11,  KC_F10,  KC_F7, _______, _______, _______, _______, _______,        _______, _______,
-        _______,   _______,   _______,                      _______,                              _______, _______, _______, _______, _______
+        _______,     RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,   KC_P7,   KC_P8,   KC_P9, KC_PMNS, _______, _______, _______,
+        _______,       _______, _______, _______, _______, RGB_VAI, RGB_VAD,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______, _______,
+        _______,            KC_F5,   S(KC_F11),  KC_F11,  KC_F10,  KC_F7, _______,   KC_P1,   KC_P2,   KC_P3, KC_PDOT,        _______, _______,
+        _______,   _______,   _______,                      _______,                                KC_P0, _______, _______, _______, _______
     ),
 
 };
